330c48f36d2d98fa02889fbf2d89edeb2661b885
Jojo Bautista <gjojob@yahoo.com>
2022-03-23 17:50:21 -0500

    Added OnTrack Group project

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,66 +1,73 @@
-import React from "react";
-import Project from "../Project";
+import React from 'react';
+import Project from '../Project';
 
 // Import the project images into a variable
-import quiz from "../../assets/images/projects/bible-character-quiz.jpg";
-import password from "../../assets/images/projects/password-generator.jpg";
-import note from "../../assets/images/projects/note-taker.jpg";
-import reddit from "../../assets/images/projects/reddit-light.jpg";
-import blog from "../../assets/images/projects/tech-blog.jpg";
-import weather from "../../assets/images/projects/weather-dashboard.jpg";
-import budget from "../../assets/images/projects/personal-budget-tracker.jpg";
-import scheduler from "../../assets/images/projects/work-day-scheduler.jpg";
+import quiz from '../../assets/images/projects/bible-character-quiz.jpg';
+import password from '../../assets/images/projects/password-generator.jpg';
+import note from '../../assets/images/projects/note-taker.jpg';
+import reddit from '../../assets/images/projects/reddit-light.jpg';
+import blog from '../../assets/images/projects/tech-blog.jpg';
+import weather from '../../assets/images/projects/weather-dashboard.jpg';
+import budget from '../../assets/images/projects/personal-budget-tracker.jpg';
+import scheduler from '../../assets/images/projects/work-day-scheduler.jpg';
+import ontrack from '../../assets/images/projects/ontrack.jpg';
 
 // List of all the projects
 export default function ProjectList() {
   const projects = [
     {
-      name: "The Tech Blog",
+      name: 'The Tech Blog',
       image: blog,
-      githubUrl: "https://github.com/jojobautistaum/tech-blog",
-      deployedUrl: "https://tranquil-ridge-17708.herokuapp.com/",
+      githubUrl: 'https://github.com/jojobautistaum/tech-blog',
+      deployedUrl: 'https://tranquil-ridge-17708.herokuapp.com/',
     },
     {
-      name: "Personal Budget Tracker",
+      name: 'Personal Budget Tracker',
       image: budget,
-      githubUrl: "https://github.com/jojobautistaum/personal-budget",
-      deployedUrl: "https://transaction-personal-budget.herokuapp.com/",
+      githubUrl: 'https://github.com/jojobautistaum/personal-budget',
+      deployedUrl: 'https://transaction-personal-budget.herokuapp.com/',
     },
     {
-      name: "Word Day Scheduler",
+      name: 'Word Day Scheduler',
       image: scheduler,
-      githubUrl: "https://github.com/jojobautistaum/work-day-scheduler",
-      deployedUrl: "https://jojobautistaum.github.io/work-day-scheduler/",
+      githubUrl: 'https://github.com/jojobautistaum/work-day-scheduler',
+      deployedUrl: 'https://jojobautistaum.github.io/work-day-scheduler/',
     },
     {
-      name: "Note Taker",
+      name: 'OnTrack Project Management',
+      image: ontrack,
+      githubUrl: 'https://github.com/Feortegas/OnTrack',
+      deployedUrl: 'https://group2-ontrack.herokuapp.com/',
+    },
+    {
+      name: 'Note Taker',
       image: note,
-      githubUrl: "https://github.com/jojobautistaum/note-taker",
-      deployedUrl: "https://safe-atoll-95629.herokuapp.com/",
+      githubUrl: 'https://github.com/jojobautistaum/note-taker',
+      deployedUrl: 'https://safe-atoll-95629.herokuapp.com/',
     },
     {
-      name: "Weather Dashboard",
+      name: 'Weather Dashboard',
       image: weather,
-      githubUrl: "https://github.com/jojobautistaum/weather-dashboard",
-      deployedUrl: "https://jojobautistaum.github.io/weather-dashboard/",
+      githubUrl: 'https://github.com/jojobautistaum/weather-dashboard',
+      deployedUrl: 'https://jojobautistaum.github.io/weather-dashboard/',
     },
     {
-      name: "Password Generator",
+      name: 'Password Generator',
       image: password,
-      githubUrl: "https://github.com/jojobautistaum/passwd-generator",
-      deployedUrl: "https://jojobautistaum.github.io/passwd-generator/",
+      githubUrl: 'https://github.com/jojobautistaum/passwd-generator',
+      deployedUrl: 'https://jojobautistaum.github.io/passwd-generator/',
     },
     {
-      name: "Bible Character Quiz",
+      name: 'Bible Character Quiz',
       image: quiz,
-      githubUrl: "https://github.com/jojobautistaum/bible-character-quiz",
-      deployedUrl: "https://jojobautistaum.github.io/bible-character-quiz/",
+      githubUrl: 'https://github.com/jojobautistaum/bible-character-quiz',
+      deployedUrl: 'https://jojobautistaum.github.io/bible-character-quiz/',
     },
     {
-      name: "Reddit Light",
+      name: 'Reddit Light',
       image: reddit,
-      githubUrl: "https://github.com/Feortegas/reddit-clone",
-      deployedUrl: "https://afternoon-tundra-62298.herokuapp.com/",
+      githubUrl: 'https://github.com/Feortegas/reddit-clone',
+      deployedUrl: 'https://afternoon-tundra-62298.herokuapp.com/',
     },
   ];
 
